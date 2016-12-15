5a093a81e4545dedf41308e229f754748c9ea666
Do Ngoc Duc <dongocduc311997@gmail.com>
2016-12-15 21:58:15 +0700

    move everything to src folder

---
+++ src/create.php
@@ -0,0 +1,62 @@
+<?php
+require 'helpers/database.php';
+require 'helpers/authenticate.php';
+
+if(!empty($_POST)) {
+	$contentError = null;
+
+	$content = $_POST['content'];
+
+	$valid = true;
+
+	if(empty($content)) {
+		$contentError = 'Please enter blog content';
+		$valid = false;
+	}
+
+	if($valid) {
+		$pdo = Database::connect();
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql = "INSERT INTO data.posts (USER_ID, CONTENT, DATE_POSTED) values(?,?,now())";
+		$q = $pdo->prepare($sql);
+		$q->execute(array($_SESSION['user_id'],$content));
+		Database::disconnect();
+		header("Location: index.php");
+	}
+}
+
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Create a Post</title>
+	<link   href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+		<div class="span10 offset1">
+			<div class="row">
+				<h3>Create a Post</h3>
+			</div>
+
+			<form action="create.php" class="form-horizontal" method="post">
+				<div class="form-group <?php echo !empty($contentError)?'error':''; ?>">
+					<label for="content">Content</label>
+					<input type="text" class="form-control" name="content" placeholder="Content" value="<?php echo !empty($content)?$content:''; ?>">
+					<?php if(!empty($contentError)): ?>
+						<span class="help-inline"><?php echo $contentError ?></span>
+					<?php endif; ?>
+				</div>
+
+				<div class="form-actions">
+					<button type="submit" class="btn btn-success">Create</button>
+					<a href="index.php" class="btn">Back</a>
+				</div>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
---
+++ src/index.php
@@ -0,0 +1,93 @@
+<?php require 'helpers/authenticate.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>My Blog</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script src="js/jquery-3.1.1.min.js"></script>
+	<script language="javascript" type="text/javascript">
+	$(document).ready(function(){
+	    $("a.delete").click(function(e){
+	        if(!confirm('Are you sure?')){
+	            e.preventDefault();
+	            return false;
+	        }
+	        return true;
+	    });
+	});
+	</script>
+
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<h3>Welcome, <?php echo $_SESSION['username'] ?></h3>
+		</div>
+		<div class="row">
+			<p>
+				<a href="create.php" class="btn btn-success">Create</a>
+				<a href="logout.php" class="btn btn-danger">Log out</a>
+			</p>
+			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<th>Username</th>
+						<th>Post content</th>
+						<th>Date posted</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					require_once 'helpers/database.php';
+					$pdo = Database::connect();
+					$sql = 'SELECT *
+									FROM data.posts JOIN data.users
+									ON data.posts.`USER_ID` = data.users.`USER_ID`
+									ORDER BY `POST_ID`';
+					foreach ($pdo->query($sql) as $row) {
+						echo '<tr class="success">';
+						echo '<td>'.$row['USERNAME'].'</td>';
+						echo '<td>'.$row['CONTENT'].'</td>';
+						echo '<td>'.$row['DATE_POSTED'].'</td>';
+						echo '<td style="background-color: white; border-color: transparent;">';
+						echo '<a class="btn btn-primary" style="margin-right:10px;" href="comment.php?post_id='.$row['POST_ID'].'">Comment</a>';
+						if($row['USERNAME'] == $_SESSION['username']) {
+							echo '<a class="btn btn-danger delete" href="delete.php?post_id='.$row['POST_ID'].'">Delete</a>';
+						}
+						echo '</td>';
+						echo '</tr>';
+
+						$sqlcomment = $pdo->prepare('SELECT *
+																				 FROM data.comments JOIN data.users
+																				 ON data.comments.`USER_ID` = data.users.`USER_ID`
+																				 WHERE `POST_ID` = ?');
+						$sqlcomment->execute(array($row['POST_ID']));
+
+						$rowcomments = $sqlcomment->fetchAll();
+
+						echo '<tr>';
+						if(empty($rowcomments)) {
+							echo '<td colspan="3"><em>No comments yet.</em></td>';
+						}
+						else {
+							echo '<td colspan="3">';
+							echo '<h4>Comments</h4>';
+							echo '<ul>';
+							foreach($rowcomments as $rowcomment) {
+								echo '<li>'.$rowcomment['COMMENT_CONTENT'].' | '.$rowcomment['USERNAME'].' | '.$rowcomment['DATE_COMMENTED'].'</li>';
+							}
+							echo '</ul>';
+							echo '</td>';
+						}
+						echo '</tr>';
+
+					}
+					Database::disconnect();
+					 ?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</body>
+</html>
