c8e9717280dcfa14f235feca6fdd9f0719b1a4c9
Do Ngoc Duc <dongocduc311997@gmail.com>
2016-12-23 17:54:40 +0700

    implement tinymce to create.php

--- src/create.php
+++ src/create.php
@@ -41,6 +41,23 @@ if(!empty($_POST)) {
 	<meta charset="UTF-8">
 	<title>Create a Post</title>
 	<link   href="css/bootstrap.min.css" rel="stylesheet">
+	<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+  <script>
+	  tinymce.init({
+	  selector: 'textarea',
+	  height: 500,
+	  theme: 'modern',
+	  plugins: [
+	    'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+	    'searchreplace wordcount visualblocks visualchars code fullscreen',
+	    'insertdatetime media nonbreaking save table contextmenu directionality',
+	    'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc'
+	  ],
+	  toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+	  toolbar2: 'print preview media | forecolor backcolor emoticons | codesample',
+	  image_advtab: true
+	 });
+ </script>
 </head>
 <body>
 	<div class="container">
